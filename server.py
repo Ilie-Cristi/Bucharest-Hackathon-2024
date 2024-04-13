b4ca0358d11b1917c21cb375e65d736b149a9aa7
catalinzz7 <raceanu.catalin@yahoo.com>
2024-04-13 13:13:14 +0300

    initial commit

---
+++ server.py
@@ -0,0 +1,11 @@
+from aiohttp import web
+
+async def handler(request):
+    return web.Response(text="Hello world")
+
+
+app = web.Application()
+app.add_routes([web.get('/', handler)])
+
+web.run_app(app)
+
