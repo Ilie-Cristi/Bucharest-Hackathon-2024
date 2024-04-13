099c9c91380aa44940582c62e196f38840eb2399
catalinzz7 <raceanu.catalin@yahoo.com>
2024-04-13 21:29:08 +0300

    + add cors

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from aiohttp import web
-
+import aiohttp_cors
 async def handler(request):
+    print("Here")
     return web.Response(text="Hello world")
 
 
@@ -16,6 +17,17 @@ async def enhance(request: web.Request) -> web.Response:
 app = web.Application()
 app.add_routes([web.get('/', handler)])
 app.add_routes([web.post('/enhance', enhance)])
+
+cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+        allow_credentials=True,
+        expose_headers="*",
+        allow_headers="*"
+    )
+})
+
+for route in list(app.router.routes()):
+    cors.add(route)
 
 web.run_app(app)
 
