9aee8edee009ffbed08f7c0e840347b0b1444fb7
catalinzz7 <raceanu.catalin@yahoo.com>
2024-04-13 14:52:59 +0300

    + post request

--- server.py
+++ server.py
@@ -4,8 +4,18 @@ async def handler(request):
     return web.Response(text="Hello world")
 
 
+async def enhance(request: web.Request) -> web.Response:
+    data = await request.post()
+    username = data.get('username')
+    print(username)
+
+    return web.Response(text=f"OK")
+
+
+
 app = web.Application()
 app.add_routes([web.get('/', handler)])
+app.add_routes([web.post('/enhance', enhance)])
 
 web.run_app(app)
 
